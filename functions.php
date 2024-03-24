11175d98d4c341dfd0b3e3c8fdbddf12c385135a
nahian28 <nahiansylhet@gmail.com>
2024-03-25 00:58:58 +0600

    Updates

--- functions.php
+++ functions.php
@@ -291,3 +291,9 @@ function hero_render_callback( $block, $content = '', $is_preview = false ) {
 	
     <?php
 }
+
+
+function my_acf_json_save_point( $path ) {
+    return get_stylesheet_directory() . '/acf-json';
+}
+add_filter( 'acf/settings/save_json', 'my_acf_json_save_point' );
